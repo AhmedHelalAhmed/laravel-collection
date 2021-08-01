4ec786300157a9c2b31d61cdd9a8b0d76d23e7ce
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-08-01 23:23:02 +0200

    Add laravel/telescope and rap2hpoutre/laravel-log-viewer

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['namespace' => '\Rap2hpoutre\LaravelLogViewer'], function()  {
+    Route::get('logs', 'LogViewerController@index');
+});
